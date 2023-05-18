1fe28d3d73f49ce8179471de3a4e05ed01bb9677
SHREYKUMAR SURESHBHAI PATEL <124682881+ShreyPatel-07@users.noreply.github.com>
2023-05-18 20:17:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const API_KEY = 'bb4a1dfe304f4fdc85e6905b52523834&ip'
+const API_KEY = 'bb4a1dfe304f4fdc85e6905b52523834&ip' //PLEASE use your API KEY as my API KEY IS limited you can find your API KEY from "https://app.ipgeolocation.io/"
 export default API_KEY
